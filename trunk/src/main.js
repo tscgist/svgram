6e424cb115a2b3748cc181864dcd5bde1b194c9e
paullasarev@gmail.com <paullasarev@gmail.com@3b80b8b1-d198-4cd8-905c-c27207dd8016>
2012-11-20 06:03:06 +0000

    initial

---
+++ trunk/src/main.js
@@ -0,0 +1,50 @@
+function SetAttrNS(element, attributes)
+{
+  for(var name in attributes)
+  {
+    element.setAttributeNS(null, name, attributes[name]);
+  }
+}
+
+function AddTagNS(parent, namespace, name, attributes)
+{
+  var tag = document.createElementNS(namespace, name);
+  parent.appendChild(tag);
+  SetAttrNS(tag, attributes);
+  return tag;
+}
+
+function CreateSvg(root, width, height, stroke)
+{
+  var svgNS = "http://www.w3.org/2000/svg";
+  var paperBorderColor = "blue";
+  var paperColor = "GhostWhite";
+  var gridStep = 20;
+  
+  var svg = AddTagNS(root, svgNS, "svg", {id:"diagram", "version":"1.1" , "width": width + 50, "height": height + 50});
+
+  var paper = AddTagNS(svg, svgNS, "g", {id:"diagram.paper"});
+
+  var defs = AddTagNS(paper, svgNS, "defs");
+  var filter = AddTagNS(defs, svgNS, "filter", {id:"shadow", width:"200%", height:"200%"});
+  AddTagNS(filter, svgNS, "feOffset", {in:"SourceAlpha", result:"offOut", dx:"8", dy:"8" });
+  AddTagNS(filter, svgNS, "feGaussianBlur", {in:"offOut", result:"blurOut", stdDeviation:"8"});
+  AddTagNS(filter, svgNS, "feBlend", {in:"SourceGraphic", in2:"blurOut", mode:"normal" });
+  
+  var border = AddTagNS(paper, svgNS, "rect", {id:"diagram.paper.border", "x":stroke, "y":stroke, 
+    "width": width - stroke * 2, "height": height - stroke * 2,
+    "fill": paperColor, "stroke": paperBorderColor, "stroke-width": stroke });
+  SetAttrNS(border, {"filter":"url(#shadow)"}); 
+  
+  var grid = AddTagNS(svg, svgNS, "g", {id:"diagram.paper.grid"});
+  for(var x = gridStep ; x < width ; x += gridStep)
+  {
+    AddTagNS(grid, svgNS, "line", {x1:x, y1:stroke, x2:x, y2: height - stroke,
+      "stroke":paperBorderColor, "stroke-width":"0.5", "stroke-dasharray": "1," + gridStep});
+  }
+}
+
+function Init(root)
+{
+  CreateSvg(root, 800, 600, 2);
+}
---
+++ trunk/test/test1.js
@@ -0,0 +1,46 @@
+test("create diagram", function() {
+  var fixture = document.getElementById("qunit-fixture");
+  var svgNS = "http://www.w3.org/2000/svg";
+  
+  equal(document.getElementById("diagram"), null, "diagram should be empty before create");
+  
+  CreateSvg(fixture, 600, 800, 2);
+  
+  var diagram = document.getElementById("diagram");
+  notEqual( diagram, null, "diagram should be not empty");
+  equal(diagram.tagName, "svg", "diagram should be SVG tag");
+
+  equal(diagram.namespaceURI, svgNS, "diagram should be SVG namespace");
+  
+  equal(diagram.getAttribute("version"), "1.1", "version should be 1.1");
+  //equal(diagram.getAttribute("width"), "600", "should have width");
+  //equal(diagram.getAttribute("height"), "800", "should have height");
+
+  paper = document.getElementById("diagram.paper");
+  notEqual(paper, null, "diagram should have paper");
+  equal(paper.tagName, "g", "paper should be a group");
+  
+  border = paper.getElementsByTagName("rect")[0];
+  notEqual(border, null, "diagram should have border");
+  
+  equal(border.getAttribute("id"), "diagram.paper.border", "border");
+  equal(border.getAttribute("x"), "2", "border.x");
+  equal(border.getAttribute("y"), "2", "border.y");
+  equal(border.getAttribute("width"), "596", "border.width");
+  equal(border.getAttribute("height"), "796", "border.hight");
+  
+  grid = document.getElementById("diagram.paper.grid");
+  notEqual(grid, null, "paper should have grid ");
+  grids = grid.getElementsByTagName("line");
+  notEqual(grids.length, 0, "grid should have lines");
+  
+});
+
+test("init tests", function() {
+  var fixture = document.getElementById("qunit-fixture");
+  
+  Init(fixture);
+  
+  notEqual(fixture.getElementsByTagName("svg")[0], null, "init should created SVG element");
+ 
+});
